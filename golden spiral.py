d9cb7cfc3fd08d31da4b4d198680236844c530bb
johncheney <71104613+johncheney@users.noreply.github.com>
2020-10-13 07:27:20 +0900

    spiral update

--- golden spiral.py
+++ golden spiral.py
@@ -32,7 +32,7 @@ def polygon(t, length, n):
 
 def arc(t, r, angle):
     arc_length = 2 * math.pi * r * abs(angle) / 360
-    n = int(arc_length / 4) + 3
+    n = int(arc_length / 4) + 30
     step_length = arc_length / n
     step_angle = float(angle) / n
     t.lt(step_angle/2)
@@ -43,23 +43,16 @@ def arc(t, r, angle):
 def circle(t, r):
     arc(t, r, 360)
 
-def fib(t, r, angle):
-    arc_length = 2 * math.pi * r * abs(angle) / 360
-    n = int(arc_length / 4) + 3
-    step_length = arc_length / n
-    step_angle = float(angle) / n
-    t.lt(step_angle/2)
-    polyline(t, n, step_length, step_angle)
-    t.rt(step_angle/2)
+
 
   
-def fibonacci(n):
+'''def fibonacci(n):
     if n == 0
         return 0
     elif n == 1
         return 1
     else:
-        return(fibonacci(n-1)+fibonacci(n-2)
+        return(fibonacci(n-1)+fibonacci(n-2)'''
         
 
 #code plan: make a list of fib numbers 
@@ -72,33 +65,39 @@ def fibonacci(n):
 
 
 
-fibo_nr = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]  
+fibo_nr = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+
 
 def draw_square(t, length):  #Function for drawing a square
     for i in range(4):
-        forward(length)
-        right(90)
+        t.fd(length)
+        t.rt(90)
 
 nr_squares=len(fibo_nr)  # I think this is just the total number of fib nums in the list (10 for our cases) 
 
-factor = 3                        #Enlargement factor  {{Re do all this so that the circle prints the proper direction. can guess and check to see which side is best to start 
-penup()
-goto(50,50)                  #Move starting point right and up {{ Ok but why is the neccessary????)) 
-pendown()
+factor = 3
+
+
+    
+    #Enlargement factor  {{Re do all this so that the circle prints the proper direction. can guess and check to see which side is best to start 
+t.penup()
+t.goto(50,50)                  #Move starting point right and up {{ Ok but why is the neccessary????)) 
+t.pendown()
 for i in range(nr_squares):
     draw_square(t, factor*fibo_nr[i]) #Draw square
-    penup()                        #Move to new corner as starting point
-    forward(factor*fibo_nr[i])
-    right(90)
-    forward(factor*fibo_nr[i])
-    pendown()
+    t.penup()                        #Move to new corner as starting point
+    t.fd(factor*fibo_nr[i])
+    t.rt(90)
+    t.fd(factor*fibo_nr[i])
+    t.pendown()
         
-penup()
-goto(50,50)       #Move to starting point
-setheading(0)   #Face the turtle to the right
-pencolor('blue')
-pensize(3)
-pendown()
+t.penup()
+t.goto(50,50)       #Move to starting point
+t.setheading(0)   #Face the turtle to the right
+t.pencolor('blue')
+t.pensize(1)
+t.pendown()
 #Draw quartercircles with fibonacci numbers as radius
 for i in range(nr_squares):
     arc(t, -factor*fibo_nr[i],90)  # minus sign to draw clockwise
